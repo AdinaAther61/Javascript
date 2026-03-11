b9961d79d0a14fc5fb3bbca21b587e79094319c8
Adina Ather <adinaather692@gmail.com>
2026-03-11 15:41:58 +0500

    commit

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+ alert(' I am inside web page')
+        console.log('hello worlsd')
+        console.log('good morning'+'adina')
+        console.warn('warning')
+        console.error('error')
+        console.clear()
+        //way of writing comment
