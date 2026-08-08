dde2158aaa414d67bd38c46969eb15c2ec02ecbd
Adina Ather <adinaather692@gmail.com>
2026-08-09 00:46:09 +0500

    Update js files

---
+++ html/first/index.js
@@ -0,0 +1,37 @@
+//1)console base api
+
+console.log("hello world",4+6);//syntax in javscript for printing
+console.warn("alert")
+console.error("eror 404")
+console.assert(4==4)//if assertion is true than nothing apppears on screen it just simply pass
+console.assert(4 == 5, "Numbers don't match!")
+// Output: Assertion failed: Numbers don't match!
+
+
+//2)variables
+var num1=5;
+var num2=8;
+console.log(num1+num2);
+//we can writ strings with double and single quotes both
+str1="dhed";
+str3='hdue';
+//objects
+var marks={a:213,b:32,c:66.6};
+console.log(marks);
+//aik sath pora object print krna hi
+//undefined
+var und;
+console.log(und)
+
+//there are two data types in javascript
+var arr=[5,5,6,6,4];
+console.log(arr)
+//ham is me string bhi le sakte
+var a4=['f,',5,6]
+console.log(a4)
+
+//operators in java script
+var a=4;
+var b=5;
+console.log(a+b)
+//console.log(!true)
